54514b24984905d2cacbecd38a818b4d3bcfaa5b
zfan71 <42745912+zfan71@users.noreply.github.com>
2019-10-20 13:13:01 -0400

    add the win condition
    add the wintext and load winscene

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public GameObject youwintext;
+    public float resetDelay = 1.0f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    public void Win()
+    {
+        youwintext.SetActive(true);
+        Time.timeScale = .5f;
+        Invoke("Reset", resetDelay);
+
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("WinScene");
+    }
+
+}
---
+++ Assets/PlayerWin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWin : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody && other.attachedRigidbody.gameObject.name.Equals("Player"))
+        {
+            GameManager.instance.Win();
+        }
+    }
+}
