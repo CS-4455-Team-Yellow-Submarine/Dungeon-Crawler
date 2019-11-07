059f4c4c71bfbd9893d580d49f34b718216f0d85
Kai Sun <kai@lawn-128-61-42-123.lawn.gatech.edu>
2019-11-07 10:15:22 -0500

    add new knight

---
+++ Assets/new_knight_controller.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class new_knight_controller : MonoBehaviour
+{
+    public bool isGrounded;
+    public bool isAttacking;
+
+    private float speed;
+    private float w_speed = 0.05f;
+    private float r_speed = 0.1f;
+    public float rotspeed;
+    Rigidbody rb;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical") * speed;
+        float y = Input.GetAxis("Horizontal") * rotspeed;
+
+
+        transform.Translate(0, 0, z);
+        transform.Rotate(0, y, 0);
+
+        if (!isAttacking)
+        {
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+
+                anim.SetTrigger("isJumping");
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = r_speed;
+                if (Input.GetKey(KeyCode.W))
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isRunning", true);
+                    anim.SetBool("isIdle", false);
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isRunning", true);
+                    anim.SetBool("isIdle", false);
+                }
+                else
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isRunning", false);
+                    anim.SetBool("isIdle", true);
+                }
+            }
+            else
+            {
+                speed = w_speed;
+                if (Input.GetKey(KeyCode.W))
+                {
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isRunning", false);
+                    anim.SetBool("isIdle", false);
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isRunning", false);
+                    anim.SetBool("isIdle", false);
+                }
+                else
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isRunning", false);
+                    anim.SetBool("isIdle", true);
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+
+            anim.SetTrigger("isAttacking");
+        }
+    }
+}
